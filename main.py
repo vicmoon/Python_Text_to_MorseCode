48af7c3a563fa5e8c1b91cf1ec583d8e230b6fcd
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2025-02-17 15:36:02 +0100

    update

---
+++ main.py
@@ -0,0 +1,41 @@
+# Transform input text to morse code  
+
+#Assign Each Letter the Morse Code Representation
+# Format the Output 
+
+
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.'
+}
+
+#Create input
+
+word = input("Enter the text: ")
+# print(word)
+
+#Separate the String into Characters  
+    # Loop through each character and replace it with its Morse equivalent. 
+morse_list = []
+
+def text_to_morse(word):
+    word = word.upper()  # Convert to uppercase
+    morse_list = []  # Store Morse code symbols
+
+    for char in word:
+        if char in MORSE_CODE_DICT:
+            morse_list.append(MORSE_CODE_DICT[char])  # Convert to Morse
+        elif char == " ":
+            morse_list.append("/")  # Word separator
+
+    return " ".join(morse_list)  # Join Morse symbols with spaces
+
+
+result = text_to_morse(word)
+print(result)
